3f3f53e600eceef59c5a6a31e422753f8b152f68
aiff <aiff@outlook.com>
2020-07-17 15:05:11 +0800

    add  widgets fail

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,14 +1,21 @@
 package controllers
 
 import javax.inject.Inject
-
+import models.Widget
 import play.api.mvc.{AbstractController, ControllerComponents}
-
+import scala.collection.mutable.ArrayBuffer
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index() = Action {
     Ok(views.html.index())
   }
-
+  private val widgets = ArrayBuffer(
+    Widget("Widget 1", 123),
+    Widget("Widget 2", 456),
+    Widget("Widget 3", 789)
+  )
+  def listWidgets = Action {
+    Ok(views.html.listWidgets(widgets.toSeq))
+  }
 
 }
