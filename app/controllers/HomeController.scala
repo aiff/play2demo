89770449d118bcf9c163c33cc195b7a9bb87de80
aiff <aiff@outlook.com>
2020-07-17 20:31:18 +0800

    add  form

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,6 +4,8 @@ import javax.inject.Inject
 import models.Widget
 import play.api.mvc.{AbstractController, ControllerComponents}
 import scala.collection.mutable.ArrayBuffer
+import play.api.data._
+import play.api.data.Forms._
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index() = Action {
@@ -14,8 +16,19 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     Widget("Widget 2", 456),
     Widget("Widget 3", 789)
   )
-  def listWidgets = Action {
-    Ok(views.html.listWidgets(widgets.toSeq))
-  }
 
+  def listWidgets = Action {
+    Ok(views.html.listWidgets(widgets.toSeq,HomeController.createWidgetForm))
+  }
+  def createWidget = TODO
+
+}
+  object HomeController
+{ val createWidgetForm = Form(
+  mapping(
+  "name" -> text,
+  "price" -> number
+  )(Widget.apply)(Widget.unapply)
+
+  )
 }
