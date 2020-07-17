319ce06eafb2b85ed4610ac9f2c0e4b9b2bbc2cf
aiff <aiff@outlook.com>
2020-07-17 14:18:45 +0800

    new

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,30 +1,13 @@
 package controllers
 
+import javax.inject.Inject
+
+import play.api.mvc.{AbstractController, ControllerComponents}
+
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index() = Action {
-    Ok(views.html.index())
+    Ok(views.html.index())..
   }
-  def  tutorial(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    // Ok(views.html.index())
 
-    Ok(generateQuote(y, scala.util.Random.nextInt(10)))
-  }
-  var y: JsValue = Json.arr(
-    Json.obj("quote" -> "views/index.scala.html  ", "author" -> "Brian Tracy"),
-    Json.obj("quote" -> "We may encounter many defeats but we must not be defeated.", "author" -> "Maya Angelou"),
-    Json.obj("quote" -> "I am not a product of my circumstances. I am a product of my decisions.", "author" -> "Stephen Covey"),
-    Json.obj("quote" -> "We must let go of the life we have planned, so as to accept the one that is waiting for us.", "author" -> "Joseph Campbell"),
-    Json.obj("quote" -> "Believe you can and you're halfway there.", "author" -> "Theodore Roosevelt"),
-    Json.obj("quote" -> "We know what we are, but know not what we may be.", "author" -> "William Shakespeare"),
-    Json.obj("quote" -> "We can't help everyone, but everyone can help someone.", "author" -> "Ronald Reagan"),
-    Json.obj("quote" -> "When you have a dream, you've got to grab it an never let go.", "author" -> "Carol Burnett"),
-    Json.obj("quote" -> "Your present circumstances don't determine where you can go; they merely determine where you start.", "author" -> "Nido Quebein"),
-    Json.obj("quote" -> "Thinking: the talking of the soul with itself.", "author" -> "Plato")
-  )
-  def generateQuote( quotes:JsValue, random:Int) : String = {
-    var quote:JsValue = (quotes(random)\"quote").get
-    var author:JsValue = (quotes(random)\"author").get
-    return (author.as[String] + ": " + quote.as[String])
-  }
 }
